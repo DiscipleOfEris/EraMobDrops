6160a4de77efc420a7a7dbdfac08992108432f9d
DiscipleOfEris <discipleoferis1@gmail.com>
2022-08-09 11:01:53 -0700

    Update parse_db.py
    Previous regex parsing was susceptible to breaking. Hopefully the new method is more future proof.

--- parse_db.py
+++ parse_db.py
@@ -5,6 +5,10 @@ import sqlite3
 con = sqlite3.connect('mobs_drops.db')
 cur = con.cursor()
 
+conTemp = sqlite3.connect(':memory:')
+conTemp.row_factory = sqlite3.Row
+curTemp = conTemp.cursor()
+
 cur.executescript('''
 DROP TABLE IF EXISTS mobs;
 DROP INDEX IF EXISTS idx_mobs_dropid;
@@ -35,7 +39,56 @@ CREATE INDEX idx_drops_itemid ON drops(item_id);''')
 
 con.commit()
 
+
+with open('sql/mob_groups.sql') as file:
+    fileContents = file.read()
+    fileContents = re.sub(r"^CREATE DATABASE[^;]*;", "", fileContents, flags=re.IGNORECASE | re.MULTILINE)
+    fileContents = re.sub(r"^USE[^;]*;", "", fileContents, flags=re.IGNORECASE | re.MULTILINE)
+    fileContents = re.sub(r"(`\w+`[^,]*)unsigned([^,]*,)", r"\1\2", fileContents, flags=re.IGNORECASE)
+    fileContents = re.sub(r"ENGINE\s?=[^;]*;", ";", fileContents, flags=re.IGNORECASE)
+    fileContents = re.sub(r"\\'", "''", fileContents)
+    curTemp.executescript(fileContents)
+
+with open('sql/mob_spawn_points.sql') as file:
+    fileContents = file.read()
+    fileContents = re.sub(r"^CREATE DATABASE[^;]*;", "", fileContents, flags=re.IGNORECASE | re.MULTILINE)
+    fileContents = re.sub(r"^USE[^;]*;", "", fileContents, flags=re.IGNORECASE | re.MULTILINE)
+    fileContents = re.sub(r"(`\w+`[^,]*)unsigned([^,]*,)", r"\1\2", fileContents, flags=re.IGNORECASE)
+    fileContents = re.sub(r"ENGINE\s?=[^;]*;", ";", fileContents, flags=re.IGNORECASE)
+    fileContents = re.sub(r"\\'", "''", fileContents)
+    curTemp.executescript(fileContents)
+
+with open('sql/mob_droplist.sql') as file:
+    fileContents = file.read()
+    fileContents = re.sub(r"^CREATE DATABASE[^;]*;", "", fileContents, flags=re.IGNORECASE | re.MULTILINE)
+    fileContents = re.sub(r"^USE[^;]*;", "", fileContents, flags=re.IGNORECASE | re.MULTILINE)
+    fileContents = re.sub(r"(`\w+`[^,]*)unsigned([^,]*,)", r"\1\2", fileContents, flags=re.IGNORECASE)
+    fileContents = re.sub(r"ENGINE\s?=[^;]*;", ";", fileContents, flags=re.IGNORECASE)
+    fileContents = re.sub(r"\\'", "''", fileContents)
+    fileContents = re.sub(',\s+KEY `dropId` \(`dropId`\)', '', fileContents, flags=re.IGNORECASE)
+    curTemp.executescript(fileContents)
+
+conTemp.commit()
+
 def mob_generator():
+  count = 0
+  for row in curTemp.execute('SELECT * FROM mob_spawn_points INNER JOIN mob_groups ON mob_spawn_points.groupid = mob_groups.groupid'):
+    count += 1
+    
+    alt_name = row['mobname'].replace('_', ' ')
+    mob_name = row['polutils_name']
+
+    if len(mob_name) == 0:
+      mob_name = alt_name
+    
+    mob_iname = re.sub('[\'\"\-\(\)\,\. ]', '', mob_name.lower())
+    
+    if count % 1000 == 0: print(count, 'mob:', row['mobid'], mob_name, mob_iname, row['zoneid'], row['dropid'], row['respawntime'], row['minLevel'], row['maxLevel'])
+      
+    yield (row['mobid'], mob_name, mob_iname, row['zoneid'], row['dropid'], row['respawntime'], row['minLevel'], row['maxLevel'])
+
+'''
+def mob_generator2():
   count = 0
   with open('sql/mob_groups.sql') as file:
     groups_str = file.read()
@@ -75,12 +128,23 @@ def mob_generator():
       if count % 1000 == 0: print(count, 'mob:', mob_id, mob_name, mob_iname, zone_id, drop_id, respawn, lvl_min, lvl_max)
       
       yield (mob_id, mob_name, mob_iname, zone_id, drop_id, respawn, lvl_min, lvl_max)
+'''
 
 cur.executemany('INSERT INTO mobs (mob_id, mob_name, mob_iname, zone_id, drop_id, respawn, lvl_min, lvl_max) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', mob_generator())
 
 con.commit()
 
 def drop_generator():
+  count = 0
+  for row in curTemp.execute('SELECT * FROM mob_droplist'):
+    count += 1
+    
+    if count % 1000 == 0: print(count, 'drop:', row['dropId'], row['dropType'], row['itemId'], row['itemRate'])
+    
+    yield (row['dropId'], row['dropType'], row['itemId'], row['itemRate'])
+    
+'''
+def drop_generator2():
   count = 0
   with open('sql/mob_droplist.sql') as file:
     dropPattern = re.compile('VALUES \((\d+), (\d+), [^,]+, [^,]+, (\d+), (\d+)\)')
@@ -99,6 +163,7 @@ def drop_generator():
       if count % 1000 == 0: print(count, 'drop:', drop_id, drop_type, item_id, item_rate)
       
       yield (drop_id, drop_type, item_id, item_rate)
+'''
 
 cur.executemany('INSERT INTO drops (drop_id, drop_type, item_id, item_rate) VALUES (?, ?, ?, ?)', drop_generator())
 
